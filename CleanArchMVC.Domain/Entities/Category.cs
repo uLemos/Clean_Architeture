2cc00dae9932c20b27a29c81e36f8cd413c51176
uLemos <fekelton2012@hotmail.com>
2023-10-12 00:38:41 -0300

    :memo: Criando os modelos de Domain

---
+++ CleanArchMVC.Domain/Entities/Category.cs
@@ -0,0 +1,40 @@
+﻿using CleanArchMVC.Domain.Validation;
+using System.Collections.Generic;
+
+namespace CleanArchMVC.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name); 
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; private set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name.Name is Required");
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
+    }
+}
