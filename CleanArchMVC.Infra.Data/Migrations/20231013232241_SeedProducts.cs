7bb5174ddd210bd064ecd81187866aa72e9cb734
uLemos <fekelton2012@hotmail.com>
2023-10-13 20:40:47 -0300

    :memo: Fazendo as migrações para a camada Infra.Data e aplicando inserções manuais com o migration

---
+++ CleanArchMVC.Infra.Data/Migrations/20231013232241_SeedProducts.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMVC.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+                "VALUES('Caderno espiral', 'Caderno espiral 100 folhas', 7.45,50, 'caderno1,jpg', 1 )");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+                "VALUES('Estojo escolar', 'Estoja escolar cinza', 5.65,70, 'estojo1,jpg', 1 )");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+            "VALUES('Borracha escolar', 'Borracha escolar branca', 5.75,70, 'borracha1,jpg', 1 )");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+            "VALUES('Calculadora escolar', 'Calculadora simples', 15.95,80, 'calculadora1,jpg', 1 )");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FORM Products");
+        }
+    }
+}
