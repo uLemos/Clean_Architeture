42cb728afac36e88f103c705dd11805ae9a89cef
uLemos <fekelton2012@hotmail.com>
2023-10-12 22:31:13 -0300

    :memo: Teste unitário utilizando xUnit e FluentAssertions para o modelo Category

---
+++ CleanArchMVC.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using CleanArchMVC.Domain.Entities;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CleanArchMVC.Domain.Tests
+{
+    public class CategoryUnitTest1
+    {
+        [Fact(DisplayName = "Create Category With Valid State")]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category Name");
+            action.Should()
+                .NotThrow<CleanArchMVC.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Category Negative Id")]
+        public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should()
+                .Throw<CleanArchMVC.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value");
+        }
+
+        [Fact(DisplayName = "Create Category Too Short Name")]
+        public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "le");
+            action.Should()
+                .Throw<CleanArchMVC.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+        [Fact(DisplayName = "Create Category Invalid Name")]
+        public void CreateCategory_WithNullNameValue_DomainExceptionInvalidName()
+        {
+            Action action = () => new Category(1, null);
+            action.Should()
+                .Throw<CleanArchMVC.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name.Name is Required");
+        }
+    }
+}
--- CleanArchMVC.Domain/Interfaces/IProductRepository.cs
+++ CleanArchMVC.Domain/Interfaces/IProductRepository.cs
@@ -1,8 +1,5 @@
 ﻿using CleanArchMVC.Domain.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchMVC.Domain.Interfaces
