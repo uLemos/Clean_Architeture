fa31b30beb46e11a4486bf433dc62c214fca9a8c
uLemos <fekelton2012@hotmail.com>
2023-10-13 19:10:11 -0300

    :memo: Implementando os serviçõs na camada IoC(Invesão de Depedência)

---
+++ CleanArchMVC.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMVC.Domain.Interfaces;
+using CleanArchMVC.Infra.Data.Context;
+using CleanArchMVC.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMVC.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+            );
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            return services;
+        }
+    }
+}
