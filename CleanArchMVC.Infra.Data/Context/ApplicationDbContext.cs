7dc2c858b8142439c259464012054919b4ea6e7b
uLemos <fekelton2012@hotmail.com>
2023-10-13 16:39:49 -0300

    :memo: Configurando a classe de contexto com o EF Core, usando o Code-First

---
+++ CleanArchMVC.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMVC.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
