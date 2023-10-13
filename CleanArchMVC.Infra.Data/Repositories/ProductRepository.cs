10bcbaf30ac30e426a0c0d44db1c9128f644ac56
uLemos <fekelton2012@hotmail.com>
2023-10-13 18:54:21 -0300

    :memo: Implementando o padrão repositório, utilizado um modelo específico e não genérico de Product

---
+++ CleanArchMVC.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿using CleanArchMVC.Domain.Entities;
+using CleanArchMVC.Domain.Interfaces;
+using CleanArchMVC.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _productContext;
+
+        public ProductRepository(ApplicationDbContext productContext)
+        {
+            _productContext = productContext;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _productContext.Add(product);
+            await _productContext.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> GetProductByIdAsync(int? id)
+        {
+            return await _productContext.Products.FindAsync(id);
+        }
+
+        public async Task<Product> GetProductCategoryAsync(int? id)
+        {
+            return await _productContext.Products.Include(x => x.Category).SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _productContext.Products.ToListAsync();
+        }
+
+        public async Task<Product> RemoveAsync(Product product)
+        {
+            _productContext.Remove(product);
+            await _productContext.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            _productContext.Update(product);
+            await _productContext.SaveChangesAsync();
+
+            return product;
+        }
+    }
+}
