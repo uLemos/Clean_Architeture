1eb6980fb5e6019aed1d11ffd7bcfc8aa01f0fc6
uLemos <fekelton2012@hotmail.com>
2023-10-14 18:32:46 -0300

    :memo: Implementando a ProductsController na camada de WebUI, que é MVC, criando views com razor pages

--- CleanArchMVC.WebUI/Startup.cs
+++ CleanArchMVC.WebUI/Startup.cs
@@ -1,14 +1,9 @@
 using CleanArchMVC.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CleanArchMVC.WebUI
 {
@@ -52,7 +47,7 @@ namespace CleanArchMVC.WebUI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Categories}/{action=Index}/{id?}");
+                    pattern: "{controller=Products}/{action=Index}/{id?}");
             });
         }
     }
