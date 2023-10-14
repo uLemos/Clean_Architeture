aa407d06335da5bbdbe5de3e3df9da065447ac3b
uLemos <fsnvag12@gmail.com>
2023-10-14 00:58:57 -0300

    :clown_face: Troca de máquina

---
+++ CleanArchMVC.Application/Interfaces/ICategoryServices.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMVC.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Application.Interfaces
+{
+    public interface ICategoryServices
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? id);
+
+        Task Add(CategoryDTO category);
+        Task Update(CategoryDTO category);
+        Task Remove(int? id);
+    }
+}
