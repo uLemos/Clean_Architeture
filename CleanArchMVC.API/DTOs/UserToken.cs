77c2d3255d4e9a2f3d2c7c69832351fa4fcd3e0e
uLemos <fekelton2012@hotmail.com>
2023-10-17 05:49:43 -0300

    :memo: Configurando as informações do TOKEN e os DTOs

---
+++ CleanArchMVC.API/DTOs/UserToken.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CleanArchMVC.API.DTOs
+{
+    public class UserToken
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
